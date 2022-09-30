472470a2b37450791ca3b0858351faea418b6a77
later_zc <2363442097@qq.com>
2022-10-01 01:07:37 +0800

    update

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const resolve = pathname => path.resolve(__dirname, pathname)
 const CracoLessPlugin = require('craco-less');
 
+
 module.exports = {
   plugins: [
     {
